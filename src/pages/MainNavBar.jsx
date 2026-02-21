10252604efc739beebe49b9c1eff5953a1aad8e9
Divyanshu Goyal <divyanshugoyal299@gmail.com>
2026-02-21 10:54:42 +0530

    modified MainNavBar.jsx

--- src/pages/MainNavBar.jsx
+++ src/pages/MainNavBar.jsx
@@ -14,7 +14,7 @@ const MainNavBar = () => {
       <div className="navbar-container">
         <div className="navbar-logo">
           <Link to="/" className="logo-link">
-            KL Event Management System
+            KL Event Management System -2500031363
           </Link>
         </div>
         <ul className="nav-menu">
